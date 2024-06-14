c6270681ab4d8eba216e928674b93aead7d9042e
Oleg Hovsepyan <hovsepyan021@gmail.com>
2024-06-15 00:43:35 +0400

    Adding tests && gas optimization

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -7,10 +7,13 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+    address USER = makeAddr('user');
+    uint256 AMOUNT_TO_SEND = 0.1 ether;
 
     function setUp() external {
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(USER, 2 ether);
     }
 
     function testMinimumDollarIsDive() public {
@@ -23,5 +26,73 @@ contract FundMeTest is Test {
 
     function testPriceFeedVersionIsCorrect() public {
         assertEq(4, fundMe.getVersion());
+    }
+
+    function testFundFailedIfNotEnoughETH() public {
+        vm.expectRevert();
+        fundMe.fund();
+    }
+
+    function testFundHappyPath() public {
+        vm.prank(USER);
+        fundMe.fund{value: AMOUNT_TO_SEND}();
+        assertEq(AMOUNT_TO_SEND, fundMe.getAddressToAmountFunded(USER));
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWithdraw() public {
+        vm.prank(msg.sender);
+        fundMe.withdraw();
+    }
+
+    function testFundAndWithdraw() public {
+        uint256 initialGas = gasleft();
+        uint256 initialBalance = msg.sender.balance;
+
+        uint160 funders = 10;
+        for(uint160 i = 1; i < funders; i++) {
+            hoax(address(i), 10 ether);
+            fundMe.fund{value: 1 ether}();
+        }
+        vm.prank(msg.sender);
+        fundMe.withdraw();
+        assertEq(9 ether, msg.sender.balance - initialBalance);
+        assertEq(0, (initialGas - gasleft()) * tx.gasprice);
+    }
+
+    function testFundAndCheapWithdraw() public {
+        uint256 initialGas = gasleft();
+        uint256 initialBalance = msg.sender.balance;
+
+        uint160 funders = 10;
+        for(uint160 i = 1; i < funders; i++) {
+            hoax(address(i), 10 ether);
+            fundMe.fund{value: 1 ether}();
+        }
+        vm.prank(msg.sender);
+        fundMe.cheaperWithdraw();
+        assertEq(9 ether, msg.sender.balance - initialBalance);
+        assertEq(0, (initialGas - gasleft()) * tx.gasprice);
+    }
+
+    function testFundAndWithdrawWithGasPrice() public {
+        vm.txGasPrice(0.001 ether);
+        uint256 initialGas = gasleft();
+        uint256 initialBalance = msg.sender.balance;
+
+        uint160 funders = 10;
+        for(uint160 i = 1; i < funders; i++) {
+            hoax(address(i), 10 ether);
+            fundMe.fund{value: 1 ether}();
+        }
+        vm.prank(msg.sender);
+        fundMe.withdraw();
+        assertEq(9 ether, msg.sender.balance - initialBalance);
+        assertNotEq(0, (initialGas - gasleft()) * tx.gasprice);
     }
 }
